a6e1ada45a1d60b46cf55233fc263192f18eff4d
Samer <sarrabi@gmail.com>
2020-05-26 19:47:45 -0700

    remove database from production settings

--- django_scorecard/settings/production.py
+++ django_scorecard/settings/production.py
@@ -4,13 +4,3 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['cryptic-falls-54884.herokuapp.com']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'scorecard',
-        'USER': 'samer',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
