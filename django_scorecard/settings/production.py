3ce354b8ea34fe9cdbc5b4d8815edeec45b1d820
Samer <sarrabi@gmail.com>
2020-07-02 09:40:02 -0700

    add www to mongolian domain

--- django_scorecard/settings/production.py
+++ django_scorecard/settings/production.py
@@ -5,6 +5,7 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['cryptic-falls-54884.herokuapp.com', 
-    'tpcprogress.com', 'gtzyavts.com', 'www.tpcprogress.com']
+    'tpcprogress.com', 'gtzyavts.com', 'www.tpcprogress.com',
+    'www.gtzyavts.com']
 
 # DATABASES = {'default': dj_database_url.config()}
