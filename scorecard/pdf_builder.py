14169aaa149462967e2e1fea6bd151f5a7bf0983
Samer <sarrabi@gmail.com>
2020-08-11 15:28:23 -0700

    add fonts, refactor pdf export

--- scorecard/pdf_builder.py
+++ scorecard/pdf_builder.py
@@ -22,9 +22,16 @@ s3_url = 'https://scorecard-static.s3-us-west-1.amazonaws.com/'
 url_loc = 'media/public/'
 file_url = s3_url + url_loc
 
-reportlab.rl_config.TTFSearchPath.append(
-    'https://s3.console.aws.amazon.com/s3/object/scorecard-static/media/public/')
-# pdfmetrics.registerFont(TTFont("Fjalla-One", "FjallaOne-Regular.ttf"))
+# reportlab.rl_config.TTFSearchPath.append(
+#     'https://scorecard-static.s3-us-west-1.amazonaws.com/media/public/'
+# )
+reportlab.rl_config.TTFSearchPath.append(str(settings.BASE_DIR))
+
+print(str(settings.BASE_DIR))
+
+pdfmetrics.registerFont(TTFont("Fjalla-One", "FjallaOne-Regular.ttf"))
+pdfmetrics.registerFont(TTFont("NotoSansMongolian", "NotoSansMongolian-Regular.ttf"))
+pdfmetrics.registerFont(TTFont("DejaVuSans", "DejaVuSans.ttf"))
 
 def _header(canvas, doc):
     # Save the state of our canvas so we can draw on it
@@ -65,80 +72,37 @@ class PageNumbers(canvas.Canvas):
     def draw_page_number(self, page_count):
         # Change the position of this to wherever you want the page number to be
         self.setFont("Helvetica", 12)
-        self.drawRightString(260 * mm, (0.4 * inch),
-                             "%d" % (self._pageNumber))
-def export_pdf(overview):
+        self.drawRightString(
+            260 * mm, (0.4 * inch),
+            str(self._pageNumber)
+        )
+
+def get_field(obj, field, language):
+    return getattr(obj, '{}_{}'.format(field, language))
+
+def export_pdf(overview, language='mn'):
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename=commitments.pdf'
         
     image = '{}Accountability+Counsel_Logo_Color+(2).png'.format(
         file_url)
-
-    #Create all different font styles
-    styles = getSampleStyleSheet()
-    header_style = styles.add(ParagraphStyle(name='header_style',
-                              fontFamily='Montserrat',
-                              fontSize=11,
-                              textColor=colors.HexColor('#FFFFFF')))
-    styleBH = ParagraphStyle('header_style', parent=styles['header_style'])
-    styleBH.alignment = TA_CENTER
-    category_style = styles.add(ParagraphStyle(name='category_style',
-                              fontFamily='Fjalla-One',
-                              fontSize=20,
-                              leftIndent=13,
-                              textColor=colors.HexColor('#FFFFFF')))
-    style_category = ParagraphStyle('category_style', parent=styles['category_style'])
-    styles.add(ParagraphStyle(name='Breadpointlist_style',
-                              fontFamily='Montserrat',
-                              fontSize=8,
-                              leading=12,
-                              wordWrap='LTR',
-                              ))
-    styleN = ParagraphStyle('bps', parent=styles['Breadpointlist_style'])
-    styles.add(ParagraphStyle(name='Title_style',
-                              fontFamily='Fjalla-One',
-                              fontSize=30,
-                              textColor=colors.HexColor("#1ca4bc")))
-    title_style = ParagraphStyle('title_style', parent=styles['Title_style'])
-    styles.add(ParagraphStyle(name='subitle1_style',
-                              fontFamily='Fjalla-One',
-                              fontSize=12))
-    subtitle1_style = ParagraphStyle('subitle1_style', parent=styles['subitle1_style'])
-    styles.add(ParagraphStyle(name='subitle2_style',
-                              fontFamily='Fjalla-One',
-                              fontSize=11))
-    subtitle2_style = ParagraphStyle('subitle2_style', parent=styles['subitle2_style'])
-
-
+    styles = get_styles()
     # Create PDF
-    pdf = SimpleDocTemplate(response, showBoundary=0, leftMargin=0.9 * cm, 
-        rightMargin=0.9*cm, topMargin=2.5*cm, bottomMargin=1*cm, pagesize=landscape(letter))
+    pdf = SimpleDocTemplate(response, showBoundary=0, 
+        leftMargin=0.9 * cm, rightMargin=0.9*cm, topMargin=2.5*cm, 
+        bottomMargin=1*cm, pagesize=landscape(letter))
 
     # Create header with titles and legend
-    story = []
-    story.append(Paragraph("<strong>{}</strong>".format(overview.name),title_style))
-    story.append(Spacer(1,.30*inch))
-    story.append(Paragraph("<strong>{}</strong>".format(overview.subtitle),subtitle1_style))
-    story.append(Spacer(1,.18*inch))
-    story.append(Paragraph("<strong>Status of Commitments as of May 2020</strong>",subtitle2_style))
-    im = Image('{}graphics.jpg'.format(file_url), width=360, height=41.076)
-    im.hAlign = 'RIGHT'
-    story.append(im)
-        
-    total_data =[]
+    story = create_header(overview, language, 
+        styles['title_style'], styles['subtitle1_style'], 
+        styles['subtitle2_style']) 
     counter = 2
     #create header row
-    row_data = []
-    for header_text in ('#', 'Commitment', 'About the Commitment',
-        'Status of Commitment', 'Original Timeline', '2019 Status',
-        '2020 Status'):
-        row_data.append(
-            Paragraph(
-                '''<b>{}</b>'''.format(header_text), styleBH
-            )
-        )
-    total_data.append(row_data)
-    table = Table(total_data, colWidths=[30, 81, 174, 290, 60, 47, 47], rowHeights=[60])
+    total_data = get_title_row(language, styles['header_style'])
+    table = Table(total_data, 
+        colWidths=[30, 81, 174, 290, 60, 47, 47], 
+        rowHeights=[60]
+    )
     styling = [
         ('BACKGROUND', (0, 0), (-1, 0), '#70747c'),
         ("GRID", (0, 0), (-1, -1), 0.2*mm, '#a0a0a4'),
@@ -149,107 +113,21 @@ def export_pdf(overview):
     table.setStyle(style)
     story.append(table)
     total_data = []
-    # Use bools to track the first instance of the category
-    category_bools = {
-        'Pasture': False,
-        'Water': False,
-        'Monitoring': False,
-        'Individual Compensation': False,
-        'Animal Husbandry Development': False,
-        'Collective Compensation': False,
-        'Undai River diversion': False,
-    }
+    category_bools = get_category_bools()
     #iterate through data
-    for commitment in overview.commitment_set.all():
-        print(commitment)
-        row_data = []
+    for commitment in overview.commitment_set \
+    .select_related('category').prefetch_related('status_set'):
         #Input the category row
-        new_category = False
-        for key, value in category_bools.items():
-            if key == commitment.category.name and value == False:
-                category_bools[key] = True
-                new_category = True
-        if new_category:
-            if counter > 2:
-                table = Table(total_data, colWidths=[30, 81, 174, 290, 60, 47, 47])
-                styling = [
-                    ("GRID", (0, 0), (-1, -1), 0.2*mm, '#a0a0a4'),
-                    ('VALIGN', (0, 0), (-1, -1), 'TOP'),
-                    ('HALIGN', (0, 0), (-1, -1), 'CENTER'),
-                    ('BOTTOMPADDING', (0, 0), (-1, -1), 7),
-                ]
-                style = TableStyle(styling)
-                table.setStyle(style)
-                story.append(table)
-                total_data = []
-                row_data = []
-            diversion_check = commitment.category.name
-            diversion_check = diversion_check.replace("diversion", "Diversion")
-            row_data.append(Paragraph(diversion_check, style_category))
-            for i in range(0, 6):
-                row_data.append(Paragraph("", style_category))
-            total_data.append(row_data)
-            table = Table(total_data, colWidths=[30, 81, 174, 290, 60, 47, 47], rowHeights=[35])
-            #[35, 95, 145, 300, 60, 45, 45]
-            styling = [
-                ("GRID", (0, 0), (-1, -1), 0.2*mm, '#a0a0a4'),
-                ('SPAN', (0, 0), (6, 0)),
-                ('VALIGN', (0, 0), (-1, -1), 'TOP'),
-                ('BACKGROUND', (0, 0), (-1, 0), '#007D8A'),
-            ]
-            style = TableStyle(styling)
-            table.setStyle(style)
-            story.append(table)
-            total_data = []
+        category_bools, new = category_header_check(
+            commitment, category_bools)
+        if new: 
+            total_data, story = add_category_header(counter, 
+                commitment, total_data, story, language, 
+                styles['category_style'])
             counter += 1
-        row_data = []
-        order = Paragraph(str(commitment.order_num) + commitment.order_letter, styleN)
-        commit = commitment.name
-        commit = commit.replace("ОТ", "OT")
-        if "href" in commit:
-            commit = commit.replace("\">", "\" color=\"#2171c7\"><u>")
-            commit = commit.replace("</a>", "</u></a>")
-        commitment_name = platypus.Paragraph(commit, styleN)
-        about_commitment = platypus.Paragraph(commitment.description, styleN)
-        status = commitment.get_status().description
-        status = status.replace('</p><p>', '<br />\n <br />\n')
-        status = status.replace("</p> <p>", '<br />\n <br />\n')
-        status = status.replace("ОТ", "OT")
-        if "href" in status:
-            status = status.replace(" http://ot.mn/reports/</a> for English", "English</a>")
-            status = status.replace("http://ot.mn/тайлан</a> for Mongolian", "Mongolian</a>")
-            status = status.replace("\">", "\" color=\"#2171c7\"><u>")
-            status = status.replace("</a>", "</u></a>")
-        status_commitment = platypus.Paragraph(status, styleN)
-        timeline = Paragraph(commitment.original_timeline, styleN)
-        statuses = commitment.status_set.order_by('date')[:2]
-        icon_url = 'https://scorecard-static.s3-us-west-1.amazonaws.com/media/public/'
-        status_displays = []
-        icon_statuses = ('not_started', 'delayed', 'in_progress', 'completed')
-        # for status in statuses:
-        #     print(status.status)
-        #     if status.status in icon_statuses:
-        #         display_status = Image(
-        #             "{0}{1}.jpg".format(
-        #                 icon_url,
-        #                 status.status, 
-        #                 width=25, height=25
-        #             )
-        #         )
-        #     else:
-        #         display_status = Paragraph(
-        #             status.get_status_display(), styleN)
-        #     status_displays.append(display_status)
-        row_data.append(order)
-        row_data.append(commitment_name)
-        row_data.append(about_commitment)
-        row_data.append(status_commitment)
-        row_data.append(timeline)
-        row_data.append('')
-        row_data.append('')
-        # row_data.append(status_displays[0])
-        # if len(status_displays) > 1:
-        #     row_data.append(status_displays[1])
+        row_data = get_commitment_row(commitment, language, 
+            styles['Breadpointlist_style'])
+        print(row_data)
         total_data.append(row_data)
     table = Table(total_data, colWidths=[30, 81, 174, 290, 60, 47, 47])
     styling = [
@@ -269,4 +147,198 @@ def export_pdf(overview):
 
     return response
 
+def get_styles():
+    #Create all different font styles
+    styles = getSampleStyleSheet()
+    styles.add(ParagraphStyle(
+        name='header_style',
+        fontName='DejaVuSans',
+        fontSize=11,
+        textColor=colors.HexColor('#FFFFFF')))
+    styles['header_style'].alignment = TA_CENTER
+    styles.add(ParagraphStyle(
+        name='category_style',
+        fontName='DejaVuSans',
+        fontSize=20,
+        leftIndent=13,
+        textColor=colors.HexColor('#FFFFFF')))
+    styles.add(ParagraphStyle(
+        name='Breadpointlist_style',
+        fontName='DejaVuSans',
+        fontSize=5,
+        leading=12,
+        wordWrap='LTR',))
+    styles.add(ParagraphStyle(
+        name='title_style',
+        fontName='DejaVuSans',
+        fontSize=30,
+        textColor=colors.HexColor("#1ca4bc")))
+    styles.add(ParagraphStyle(
+        name='subtitle1_style',
+        fontName='DejaVuSans',
+        fontSize=12))
+    styles.add(ParagraphStyle(
+        name='subtitle2_style',
+        fontName='DejaVuSans',
+        fontSize=11))
+    return styles
 
+def create_header(overview, language, title_style, style1, style2):
+    story = []
+    story.append(Paragraph("<strong>{}</strong>".format(
+        get_field(overview, 'name', language)), 
+        title_style)
+    )
+    story.append(Spacer(1,.30*inch))
+    story.append(Paragraph("<strong>{}</strong>".format(
+        get_field(overview, 'subtitle', language)), style1))
+    story.append(Spacer(1,.18*inch))
+    if language == 'mn':
+        h3 = "<strong>Үүрэг Амлалтын Хэрэгжилтийн Байдал 2020</strong>"
+    else:
+        h3 = "<strong>Status of Commitments as of May 2020</strong>"
+    story.append(Paragraph(h3, style2))
+    im = Image('{}graphics.jpg'.format(file_url), width=360, height=41.076)
+    im.hAlign = 'RIGHT'
+    story.append(im)
+    return story
+
+def get_title_row(language, style):
+    row_data = []
+    if language == 'mn':
+        headers = ('#', 'Үүрэг Амлалтууд', 
+            'нэ Үүрэг Амлалтын Тухай', 'Үүрэг Амлалтын Хэрэгжилтийн Байдал',
+            'Анх Товлосон Хугацаа', '2019', '2020'
+        )
+    else:
+        header = ('#', 'Commitment', 'About the Commitment',
+        'Status of Commitment', 'Original Timeline', '2019 Status',
+        '2020 Status')
+    for header_text in headers:
+        row_data.append(
+            Paragraph(
+                '''<b>{}</b>'''.format(header_text), style
+            )
+        )
+    return [row_data,]
+
+def get_category_bools():
+    return {
+        'Pasture': False,
+        'Water': False,
+        'Monitoring': False,
+        'Individual Compensation': False,
+        'Animal Husbandry Development': False,
+        'Collective Compensation': False,
+        'Undai River Diversion': False,
+    }
+
+def category_header_check(commitment, category_bools):
+    # Use bools to track the first instance of the category
+    new_category = False
+    for key, value in category_bools.items():
+        if key == commitment.category.name and value == False:
+            category_bools[key] = True
+            new_category = True
+    if new_category:
+        return category_bools, True
+    return category_bools, False
+
+def add_category_header(counter, commitment, total_data, 
+    story, language, style):
+    if counter > 2:
+        table = Table(total_data, colWidths=[30, 81, 174, 290, 60, 47, 47])
+        styling = [
+            ("GRID", (0, 0), (-1, -1), 0.2*mm, '#a0a0a4'),
+            ('VALIGN', (0, 0), (-1, -1), 'TOP'),
+            ('HALIGN', (0, 0), (-1, -1), 'CENTER'),
+            ('BOTTOMPADDING', (0, 0), (-1, -1), 7),
+        ]
+        table.setStyle(TableStyle(styling))
+        story.append(table)
+        total_data = []
+    row_data = []
+    row_data.append(Paragraph(
+        get_field(commitment.category, 'name', language), 
+        style))
+    for i in range(0, 6):
+        row_data.append(Paragraph("", style))
+    total_data.append(row_data)
+    table = Table(total_data, colWidths=[30, 81, 174, 290, 60, 47, 47], 
+        rowHeights=[35])
+    #[35, 95, 145, 300, 60, 45, 45]
+    styling = [
+        ("GRID", (0, 0), (-1, -1), 0.2*mm, '#a0a0a4'),
+        ('SPAN', (0, 0), (6, 0)),
+        ('VALIGN', (0, 0), (-1, -1), 'TOP'),
+        ('BACKGROUND', (0, 0), (-1, 0), '#007D8A'),
+    ]
+    table.setStyle(TableStyle(styling))
+    story.append(table)
+    total_data = []
+    return total_data, story
+
+def get_commitment_row(commitment, language, style):
+    order = Paragraph(
+        str(commitment.order_num) + commitment.order_letter,
+        style)
+    name = get_commitment_name(commitment, language, style)
+    description = platypus.Paragraph(
+        get_field(commitment, 'description', language), style)
+    status = get_commitment_status(commitment, language, style)
+    timeline = Paragraph(
+        get_field(commitment, 'original_timeline', language), 
+        style
+    )
+    statuses = commitment.status_set.order_by('date')[:2]
+    row_data = [order, name, description, status, timeline]
+    row_data.extend(get_status_displays(statuses, style))
+    return row_data
+
+def get_commitment_name(commitment, language, style):
+    name = get_field(commitment, 'name', language)
+    if "href" in name:
+        name = name.replace("\">", "\" color=\"#2171c7\"><u>")
+        name = name.replace("</a>", "</u></a>")
+    return platypus.Paragraph(name, style)
+
+def get_commitment_status(commitment, language, style):
+    status = get_field(commitment.get_status(), 'description', 
+        language)
+    status = clean_status(status)
+    return platypus.Paragraph(status, style)
+
+def clean_status(status):
+    status = status.replace('</p><p>', '<br />\n <br />\n')
+    status = status.replace("</p> <p>", '<br />\n <br />\n')
+    status = status.replace("ОТ", "OT")
+    if "href" in status:
+        status = status.replace(" http://ot.mn/reports/</a> for English", 
+            "English</a>")
+        status = status.replace("http://ot.mn/тайлан</a> for Mongolian", 
+            "Mongolian</a>")
+        status = status.replace("\">", "\" color=\"#2171c7\"><u>")
+        status = status.replace("</a>", "</u></a>")
+    return status
+
+def get_status_displays(statuses, style):
+    # status_displays = []
+    # icon_statuses = ('not_started', 'delayed', 
+    #     'in_progress', 'completed')
+    # for status in statuses:
+    #     if status.status in icon_statuses:
+    #         display_status = Image(
+    #             "{0}{1}.jpg".format(
+    #                 file_url,
+    #                 status.status, 
+    #                 width=25, height=25
+    #             )
+    #         )
+    #     else:
+    #         display_status = Paragraph(
+    #             status.get_status_display(), style)
+    #     status_displays.append(display_status)
+    # if len(status_displays) < 2:
+    #     status_displays.append('')
+    # return status_displays
+    return ['','']
